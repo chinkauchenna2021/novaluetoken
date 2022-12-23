57483424825687b271110562383781c7080da89b
chinkauchenna20211 <uchennachinka2021@gmail.com>
2022-12-23 13:16:30 +0100

    added novaluetokenss

--- platform/contracts/dev/dummy/DummyUSDTether.sol
+++ platform/contracts/dev/dummy/DummyUSDTether.sol
@@ -11,36 +11,112 @@ contract DummyUSDTether is ERC20{
       event Withdrawal(address indexed to, uint256 indexed amount);
       event Deposit(address indexed from, uint256 indexed amount);
       mapping(address => uint256) public  nextAccessTime;  
+      uint256 private startTime;
+      uint256 timelapse  = 1 minutes;
+      event Timelapse(string);
+
+   
   constructor() ERC20("USD Tether (Dummy)", "USDT"){  
     owner = payable(msg.sender);  
+     startTime = block.timestamp;
+    listOfOwners.push(payable(msg.sender));
     _mint(payable(msg.sender), (100000000000000000 * (10 ** 6)));
   }
+
+
+address payable []  listOfOwners = [owner] ;
 
 function decimals() public pure override returns(uint8) {
   return uint8(6);
 }
 
+function addOwners(address payable owners) public onlyOwner returns(uint256) {
+    listOfOwners.push(owners);
+    return listOfOwners.length;
+}
 
- function mint(address to , uint256 amount) public onlyOwner {
- require(to != address(0)," zero account");
- require((amount) <= (1000 * (10 ** 6)),"amount <=1000");
- require(block.timestamp >= nextAccessTime[to],"time not elapsed ");
- nextAccessTime[to] = block.timestamp + lockTime;
- _mint(to,(amount));
+
+function getUserId(address _addr) public view  onlyOwner returns(int256 , string memory){
+int256 _numb  = -1;
+string memory text = "not available";
+     for(uint256 i = 0 ; i < listOfOwners.length - 1 ; i++){
+           if(listOfOwners[i] == payable(_addr)){
+            _numb = int256(i);
+             text = "available";
+           }
+     }
+     return (_numb , text);
+}
+
+
+
+function deleteUserWhitelist (uint256 _pos) public  onlyOwner returns(bool){
+         require((listOfOwners[_pos] != address(0)), "Address not Authorized");
+         uint lastPayeeIndex= listOfOwners.length-1;
+        address lastPayeeId=listOfOwners[lastPayeeIndex];
+        listOfOwners[_pos]= payable(lastPayeeId);
+        listOfOwners.pop();
+        return true ; 
+
+
+}
+
+
+function isAddrssWhilelisted() public view returns(bool) {
+      bool isWhitelisted  ;
+   for(uint256 i = 0 ; i < listOfOwners.length ; i++){
+        if(listOfOwners[i] == (payable(msg.sender))){
+          isWhitelisted = true;
+        }else{
+         isWhitelisted = false;
+        }
+
+   }
+ return isWhitelisted;
+}
+
+
+
+
+ function mint(uint256 amount) public{
+   require(((isAddrssWhilelisted() != false) || (owner == msg.sender)), "This address is not whitelisted" );
+  _mint(payable(msg.sender),amount);
  }
+
+
+function checkAddressBalance (address _address) public  view returns(uint256){
+ return (balanceOf(_address));
+}
+
+
+function setTimelap( uint256 _timeinMinutes)public onlyOwner returns(bool){
+  uint256 lap = (_timeinMinutes * 1 minutes);
+  timelapse = lap;
+  return true ;
+
+}
+
+
 
 function transfers(address to , uint256 amount ) public {
 require(to != address(0)," zero account");
  require((amount) <= (1000 * (10 ** 6)),"amount <=1000");
+ if(startTime >= block.timestamp){
+ emit Timelapse("queue too long , check back in a few minutes");
+ revert();
+ }else{
  require(block.timestamp >= nextAccessTime[to],"time not elapsed ");
  nextAccessTime[to] = block.timestamp + lockTime;
  transfer(payable(to), amount);
+ startTime = (block.timestamp + timelapse);
+
+ }
 }
 
 
 
 function getBalance() external view returns  (uint256){
- return balanceOf(address(this));
+ return balanceOf(msg.sender);
  }
  function setLockTime(uint256 amount) public onlyOwner {
  lockTime = amount * 1 minutes;
